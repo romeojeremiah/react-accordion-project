e330ff2d65e5671a8cb68994c94f4d9447866858
Romeo Jeremiah <romeoclayton@gmail.com>
2022-08-21 16:14:10 -0400

    Add files via upload

---
+++ precache-manifest.c2a30687a824afb82bac4c6cd75dc6b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4e747422a4c495be1fd2dad7b9606e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcc803db990025fa02e5",
+    "url": "/static/css/main.071378ba.chunk.css"
+  },
+  {
+    "revision": "11e2b97d487a12b7b136",
+    "url": "/static/js/2.f745c462.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f745c462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc803db990025fa02e5",
+    "url": "/static/js/main.8666d3eb.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
